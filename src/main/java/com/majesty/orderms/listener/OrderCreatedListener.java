bcf955b4d3b79a7f8a06002748c096be001023fc
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-01-28 11:28:23 -0300

    Feat: Add listener for rabbitmq

---
+++ src/main/java/com/majesty/orderms/listener/OrderCreatedListener.java
@@ -0,0 +1,33 @@
+package com.majesty.orderms.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import org.springframework.messaging.Message;
+
+import com.majesty.orderms.listener.dto.OrderCreatedEvent;
+import com.majesty.orderms.service.OrderService;
+
+import static com.majesty.orderms.config.RabbitMqConfig.ORDER_CREATED_QUEUE;
+
+@Component
+public class OrderCreatedListener {
+
+    private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
+    private final OrderService orderService;
+
+    public OrderCreatedListener(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @RabbitListener(queues = ORDER_CREATED_QUEUE)
+    public void listen(Message<OrderCreatedEvent> message) {
+        logger.info("Message consumed: {}", message);
+
+        orderService.save(message.getPayload());
+
+    }
+
+}
